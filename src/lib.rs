5b8b69ea0bfbad8ded84518009382b064ea43d84
AlessandroRuggiero <50781152+AlessandroRuggiero@users.noreply.github.com>
2025-09-02 20:58:22 +0200

    Add direction calculation to RSI output and update function parameters

--- src/lib.rs
+++ src/lib.rs
@@ -9,12 +9,17 @@ use ta::{Next, indicators::RelativeStrengthIndex};
 pub struct Output {
     ticker: String,
     rsi: f64,
+    direction: f64,
 }
 
 #[plugin_fn]
 pub fn run(fin_data: FinData) -> FnResult<Output> {
     let ticker = fin_data.get_ticker("pegged_data")?;
     let period = fin_data.get_call_argument::<usize>("period")?;
+    let rsi_low = fin_data.get_call_argument::<f64>("rsi_low").unwrap_or(14.0);
+    let rsi_high = fin_data
+        .get_call_argument::<f64>("rsi_high")
+        .unwrap_or(70.0);
     let mut rsi = RelativeStrengthIndex::new(period).unwrap();
     let mut last = 0.0;
 
@@ -22,8 +27,17 @@ pub fn run(fin_data: FinData) -> FnResult<Output> {
         last = rsi.next(price.close);
     }
 
+    let direction = if last < rsi_low {
+        -1.0
+    } else if last > rsi_high {
+        1.0
+    } else {
+        0.0
+    };
+
     Ok(Output {
         ticker: ticker.symbol.clone(),
         rsi: last,
+        direction,
     })
 }
