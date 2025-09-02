a699f68938a7cc5ce5a013314b240a2864496536
AlessandroRuggiero <50781152+AlessandroRuggiero@users.noreply.github.com>
2025-09-02 21:31:58 +0200

    Refactor RSI calculation to include email alert functionality and update output structure

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
 mod exchange_outpost;
-use crate::exchange_outpost::FinData;
+use crate::exchange_outpost::{FinData, schedule_email};
 use extism_pdk::{FnResult, Json, ToBytes, encoding, plugin_fn};
 use serde::Serialize;
 use ta::{Next, indicators::RelativeStrengthIndex};
@@ -10,18 +10,23 @@ pub struct Output {
     ticker: String,
     rsi: f64,
     direction: f64,
+    email_sent: bool,
 }
 
 #[plugin_fn]
 pub fn run(fin_data: FinData) -> FnResult<Output> {
     let ticker = fin_data.get_ticker("pegged_data")?;
-    let period = fin_data.get_call_argument::<usize>("period")?;
-    let rsi_low = fin_data.get_call_argument::<f64>("rsi_low").unwrap_or(14.0);
+    let period = fin_data.get_call_argument::<usize>("period").unwrap_or(14);
+    let rsi_low = fin_data.get_call_argument::<f64>("rsi_low").unwrap_or(20.0);
     let rsi_high = fin_data
         .get_call_argument::<f64>("rsi_high")
         .unwrap_or(70.0);
+    let email = fin_data
+        .get_call_argument::<String>("email")
+        .unwrap_or("".to_string());
     let mut rsi = RelativeStrengthIndex::new(period).unwrap();
     let mut last = 0.0;
+    let mut email_sent = false;
 
     for price in ticker.candles.iter() {
         last = rsi.next(price.close);
@@ -35,9 +40,19 @@ pub fn run(fin_data: FinData) -> FnResult<Output> {
         0.0
     };
 
+    if direction != 0.0 && !email.is_empty() {
+        let message = format!(
+            "RSI alert for {}: RSI value is {:.2}, direction is {}.",
+            ticker.symbol, last, direction
+        );
+        schedule_email(&email, message.as_str())?;
+        email_sent = true;
+    }
+
     Ok(Output {
         ticker: ticker.symbol.clone(),
         rsi: last,
         direction,
+        email_sent,
     })
 }
