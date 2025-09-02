4e149e5e29ac77a60e3cd8c5f69cd17457c0b4d4
AlessandroRuggiero <50781152+AlessandroRuggiero@users.noreply.github.com>
2025-09-02 20:42:47 +0200

    .

--- src/lib.rs
+++ src/lib.rs
@@ -5,9 +5,18 @@ use serde::Serialize;
 
 #[derive(Serialize, ToBytes)]
 #[encoding(Json)]
-pub struct Output {}
+pub struct Output {
+    ticker: String,
+    period: usize,
+}
 
 #[plugin_fn]
 pub fn run(fin_data: FinData) -> FnResult<Output> {
-    Ok(Output {})
+    let ticker = fin_data.get_ticker("pegged_data")?;
+    let period = fin_data.get_call_argument::<usize>("period")?;
+
+    Ok(Output {
+        ticker: ticker.symbol.clone(),
+        period,
+    })
 }
